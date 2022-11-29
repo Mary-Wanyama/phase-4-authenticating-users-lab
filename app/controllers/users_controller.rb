34a073d73094c8b026eace08f0af8e1c351e5aa7
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-29 15:24:59 +0300

    completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def show
         if session[:user_id]
             user = User.find(session[:user_id])
-            render json: user
+            render json: user, status: :ok
         else
             render json: { error: "User not authorized" }, status: :unauthorized
         end
